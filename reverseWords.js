e591c31a70f352e1104ffbaa90296fe2dbdf19d7
dimacellist <dyanovyanovsky@gmail.com>
2019-09-10 22:24:28 +0300

    ReverseWords inprovements

--- reverseWords.js
+++ reverseWords.js
@@ -1,7 +1,5 @@
 function reverse_words(str) {
-  str = ""
-  let reversed = str.split('').reverse().join('')
-  return reversed
+  return str.split('').reverse().join('')
 }
 
 reverse_words("Marry got married to a goat")
